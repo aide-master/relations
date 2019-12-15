83aaedcbd1a50e6359a253932eac007079bb2728
Yudu Ban <banyudu@gmail.com>
2019-12-15 12:37:28 +0800

    chore: setup eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,10 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      mocha: true,
-      jest: true
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true
-      },
-      useJSXTextNode: true
-    },
-    extends: ['plugin:@typescript-eslint/recommended'],
-    plugins: ['react', 'jest', 'react-hooks', '@typescript-eslint', 'i18n'],
-    rules: {},
-    settings: {
-      react: {
-        version: '999.999.999'
-      }
-    },
-    globals: {}
+  extends: ['standard-with-typescript', 'standard-react'],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  env: {
+    node: true,
+    jest: true
   }
-  
+}
--- packages/backend/handler.ts
+++ packages/backend/handler.ts
@@ -1,12 +1,13 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from 'aws-lambda'
+import 'source-map-support/register'
 
 export const hello: APIGatewayProxyHandler = async (event, _context) => {
+  await 1
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+      input: event
+    }, null, 2)
+  }
 }
