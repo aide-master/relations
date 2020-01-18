80e84d56b011ce0e17ed372930c0a1ff18643d77
Yudu Ban <banyudu@gmail.com>
2020-01-18 10:29:11 +0800

    chore: add title in home page

--- packages/wiki/pages/index.tsx
+++ packages/wiki/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Head from 'next/head'
 import './index.css'
 import SearchBar from '../components/search-bar'
 
 const App: React.FC = () => {
   return (
     <div className='App'>
+      <Head>
+        <title>Relations</title>
+      </Head>
       <header className='App-header'>
         <img src='/logo.svg' className='App-logo' alt='logo' />
         <SearchBar />
