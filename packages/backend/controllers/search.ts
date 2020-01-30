a5ead3de8077830e785f923f1bf478bb3ba73a52
Yudu Ban <banyudu@gmail.com>
2020-01-31 02:02:55 +0800

    chore: query database before query wikipedia

--- packages/backend/controllers/search.ts
+++ packages/backend/controllers/search.ts
@@ -9,14 +9,23 @@ export const search: APIGatewayProxyHandler = run(async (event, _context) => {
     word: Joi.string().required(),
     lang: Joi.string()
   }))
-  const url = getWikiUrl(word, lang)
-  const extract = await getExtract(word, lang)
-  const htmlRes = await rest.get(url)
-  const result = getLinksFromHtml(htmlRes.data, word)
-  const relations = Object.keys(result).sort((a, b) => result[b] - result[a]).map(item => [item, result[item]])
-  if (extract && relations.length) {
-    const rec = new Relation({ name: word, lang, extract, relations: relations.map(item => ({ key: item[0], value: item[1] })) })
-    await rec.save()
+
+  let extract: string
+  let relations: Array<[string, number]>
+  const dbRec: any = await Relation.get({ name: word, lang })
+  if (dbRec) {
+    extract = dbRec.extract
+    relations = (dbRec.relations || []).map(item => [item.key, item.value])
+  } else {
+    const url = getWikiUrl(word, lang)
+    extract = await getExtract(word, lang)
+    const htmlRes = await rest.get(url)
+    const result = getLinksFromHtml(htmlRes.data, word)
+    relations = Object.keys(result).sort((a, b) => result[b] - result[a]).map(item => [item, result[item]])
+    if (extract && relations.length) {
+      const rec = new Relation({ name: word, lang, extract, relations: relations.map(item => ({ key: item[0], value: item[1] })) })
+      await rec.save()
+    }
   }
   return {
     statusCode: 200,
